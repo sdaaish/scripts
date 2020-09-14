85827ab9c9d9aa1b62f48f2124de2081d9b7229d
Stig Dahl <stig@charlottendal.net>
2020-09-14 17:57:47 +0200

    Corrected renamed files

--- wsb/Sandbox-Install.cmd
+++ wsb/Sandbox-Install.cmd
@@ -2,4 +2,4 @@
 
 :: Settings for powershell
 powershell -Command Set-ExecutionPolicy -ExecutionPolic RemoteSigned -Scope CurrentUser
-powershell -File C:\users\WDAGUtilityAccount\Desktop\Scripts\wsb\install-sandbox.ps1
+powershell -File C:\users\WDAGUtilityAccount\Desktop\Scripts\wsb\Sandbox-Install.ps1
