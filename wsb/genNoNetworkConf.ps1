91b8ceeb89f87a4bd9856a370fb39771fc3e80d3
Stig Dahl <stig@charlottendal.net>
2020-10-17 22:56:53 +0200

    Add a config without networking

---
+++ wsb/genNoNetworkConf.ps1
@@ -0,0 +1,50 @@
+<#
+.SYNOPSIS
+Generates configuration for Windows Sandbox without networking.
+
+.DESCRIPTION
+Run this to create files to be able to start the installation process in Windows Sandbox.
+
+.PARAMETER Foobar
+Descriptions of parameter Foobar
+
+.EXAMPLE
+./genNoNetworkConf.ps1
+
+.NOTES
+Run a sandbox that maps files from the download folder. For checking out bad code as an example. Without networking
+#>
+
+$scriptFolder = Split-Path -Path $PSScriptroot -Parent
+$wsbFile = Join-Path -Path $PSScriptRoot -ChildPath "NoNetwork.wsb"
+$downloadFolder = New-Item ~/Downloads -ItemType Directory -Force -ErrorAction Ignore
+$scoopFolder = Convert-Path ~/scoop
+
+# Generate the wsb-file to use to start the sandbox from Windows (outside).
+$wsbContent = @"
+<Configuration>
+<VGpu>Default</VGpu>
+<Networking>Disabled</Networking>
+<MappedFolders>
+   <MappedFolder>
+<HostFolder>${downloadFolder}</HostFolder>
+<ReadOnly>true</ReadOnly>
+   </MappedFolder>
+   <MappedFolder>
+     <HostFolder>${scriptFolder}</HostFolder>
+     <ReadOnly>true</ReadOnly>
+   </MappedFolder>
+   <MappedFolder>
+<HostFolder>${scoopFolder}</HostFolder>
+<ReadOnly>true</ReadOnly>
+   </MappedFolder>
+</MappedFolders>
+<LogonCommand>
+</LogonCommand>
+</Configuration>
+"@
+
+Set-Content -Path $wsbFile -Value $wsbContent -Force
+
+Write-Output "Generated Windows sandbox file in ${wsbFile}."
+Write-Output "Start sandbox with `"& .\${wsbFile}.`""
--- wsb/setup.ps1
+++
@@ -1,6 +0,0 @@
-$homedir = Resolve-Path ~
-$sandbox = Join-Path -Path ~/Downloads -ChildPath Sandbox
-New-Item -Path $sandbox -ItemType Directory
-
-New-Item -Path \Local -ItemType Directory
-New-Item -Path \Local -ItemType Directory
