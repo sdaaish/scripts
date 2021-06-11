35a49640e9b7f92f283e8e0f6496363c69a77d3d
Stig Dahl <stig@charlottendal.net>
2021-06-11 17:39:05 +0200

    Updated scripts and added software

--- wsb/genSupportConf.ps1
+++ wsb/genSupportConf.ps1
@@ -9,18 +9,17 @@ Run this to create files to be able to start the installation process in Windows
 Descriptions of parameter Foobar
 
 .EXAMPLE
-./gen-sandboxConf.ps1
+./gen-supportConf.ps1
 
 .NOTES
 Run a sandbox that maps files from the download folder. For checking out bad code as an example.
 #>
 
 $scriptFolder = Split-Path -Path $PSScriptroot -Parent
-$wsbFile = Join-Path -Path $PSScriptRoot -ChildPath "Badware.wsb"
-$cmdFile = Join-Path -Path $PSScriptRoot -ChildPath "Badware.cmd"
-$poshFile = Join-Path -Path $PSScriptRoot -ChildPath "Badware.ps1"
+$wsbFile = Join-Path -Path $PSScriptRoot -ChildPath "Support.wsb"
+$cmdFile = Join-Path -Path $PSScriptRoot -ChildPath "Support.cmd"
+$poshFile = Join-Path -Path $PSScriptRoot -ChildPath "Support.ps1"
 $sandboxFolder = New-Item ~/Downloads/Sandbox -ItemType Directory -Force -ErrorAction Ignore
-$sandboxFolderRW = New-Item ~/Downloads/SandboxRW -ItemType Directory -Force -ErrorAction Ignore
 $scoopFolder = Convert-Path ~/scoop
 $sandboxHome = "C:\Users\WDAGUtilityAccount"
 
@@ -29,25 +28,21 @@ $cmdContent = @"
 @echo off
 
 :: Settings for powershell
-powershell -Command {Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser}
-powershell -ExecutionPolicy ByPass -File C:\users\WDAGUtilityAccount\Desktop\Scripts\wsb\Badware.ps1
+powershell -ExecutionPolicy ByPass -Command {Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser}
+powershell -ExecutionPolicy ByPass -File C:\users\WDAGUtilityAccount\Desktop\Scripts\wsb\Support.ps1
 "@
 
 # Content of Powershell script to use _inside_ sandbox
 $poshContent = @"
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser -Force
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
 scoop install git
 scoop bucket add extras
 scoop install vscode
-scoop install firefox
-scoop install curl
-reg import "${sandboxHome}\scoop\apps\vscode\current\vscode-install-context.reg"
-Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-Install-Module -Name DNSClient-PS -Force
-curl.exe -o ${sandboxHome}\Downloads\CyperChef.zip https://gchq.github.io/CyberChef/CyberChef_v9.28.0.zip
-Expand-Archive ${sandboxHome}\Downloads\CyperChef.zip -DestinationPath ${sandboxHome}\Desktop\CyberChef
-Remove-Item ${sandboxHome}\Downloads\CyperChef.zip -Force
+scoop install putty
+scoop install googlechrome
+scoop install windows-terminal
+reg import "C:\Users\WDAGUtilityAccount\scoop\apps\vscode\current\vscode-install-context.reg"
 Import-Module ${sandboxHome}\Desktop\scripts\wsb\Modules\My-Explorer.psm1
 My-Explorer
 "@
@@ -55,18 +50,12 @@ My-Explorer
 # Generate the wsb-file to use to start the sandbox from Windows (outside).
 $wsbContent = @"
 <Configuration>
- <VGpu>Default</VGpu>
- <Networking>Default</Networking>
- <MappedFolders>
-  <MappedFolder>
+<MappedFolders>
+<MappedFolder>
 <HostFolder>${sandboxFolder}</HostFolder>
 <ReadOnly>true</ReadOnly>
-</MappedFolder>
-<MappedFolder>
-<HostFolder>${sandboxFolderRW}</HostFolder>
-<ReadOnly>false</ReadOnly>
-</MappedFolder>
-<MappedFolder>
+ </MappedFolder>
+ <MappedFolder>
   <HostFolder>${scriptFolder}</HostFolder>
   <ReadOnly>true</ReadOnly>
  </MappedFolder>
@@ -76,14 +65,14 @@ $wsbContent = @"
  </MappedFolder>
  </MappedFolders>
   <LogonCommand>
-<Command>cmd.exe /c C:\users\WDAGUtilityAccount\Desktop\scripts\wsb\Badware.cmd</Command>
+<Command>cmd.exe /c C:\users\WDAGUtilityAccount\Desktop\scripts\wsb\Support.cmd</Command>
 </LogonCommand>
 </Configuration>
 "@
 
 Set-Content -Path $wsbFile -Value $wsbContent -Encoding utf8 -Force
 Set-Content -Path $cmdFile -Value $cmdContent -Encoding utf8 -Force
-Set-Content -Path $poshFile -Value $poshContent -Encoding utf8  -Force
+Set-Content -Path $poshFile -Value $poshContent -Encoding utf8 -Force
 
 Write-Output "Generated Windows sandbox file in ${wsbFile}."
 Write-Output "Start sandbox with `"& .\${wsbFile}.`""
