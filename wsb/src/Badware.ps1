41931db8402189b443c0e0e32f3cb674970a9d68
Stig Dahl <stig@charlottendal.net>
2021-06-18 15:01:11 +0200

    Add wireshark, nmap

--- wsb/src/Badware.ps1
+++ wsb/src/Badware.ps1
@@ -12,8 +12,15 @@ scoop install vscode
 scoop install firefox
 scoop install curl
 scoop install networkminer
+scoop install wireshark
+scoop install nmap
 
+# Install VSCode settings
 reg import "${env:USERPROFILE}\scoop\apps\vscode\current\vscode-install-context.reg"
+
+# Enable debug of SSL in Firefox, Chrome and Wireshark
+$LogFile = Join-Path ${env:USERPROFILE} "Downloads\ssl\ssl.log"
+[System.Environment]::SetEnvironmentVariable("SSLKEYLOGFILE",$LogFile,"USER")
 
 # Install PS-modules
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
@@ -27,7 +34,3 @@ Remove-Item ${env:USERPROFILE}\Downloads\CyperChef.zip -Force
 # Local explorer settings
 Import-Module ${env:USERPROFILE}\Desktop\wsb\Modules\My-Explorer.psm1
 My-Explorer
-
-# Enable debug of SSL in Firefox, Chrome and Wireshark
-$LogFile = Join-Path ${env:USERPROFILE} "Downloads\ssl\ssl.log"
-[System.Environment]::SetEnvironmentVariable("SSLKEYLOGFILE",$LogFile,"USER")
