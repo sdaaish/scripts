7328cbfad1620589679194447d80050879aab157
Stig Dahl <stig@charlottendal.net>
2021-10-16 23:27:16 +0200

    Download brim, dynamic version also for CyberChef

--- wsb/src/Badware.ps1
+++ wsb/src/Badware.ps1
@@ -27,18 +27,27 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name DNSClient-PS -Force
 
 # Download CyberChef
-$ccversion = "9.32.1"
-$cyberchefUrl = "https://gchq.github.io/CyberChef/CyberChef_v${ccversion}.zip"
-$ccfile = Join-Path ${env:USERPROFILE} "Downloads\CyberChef_v${ccversion}.zip"
+$ccVersion = Invoke-RestMethod "https://api.github.com/repos/gchq/CyberChef/releases/latest"
+$ccTag = ($ccVersion).tag_name
+$cyberchefUrl = ($ccVersion).assets.browser_download_url
+$ccfile = Join-Path ${env:USERPROFILE} "Downloads\CyberChef_${ccTag}.zip"
 (New-Object System.Net.WebClient).DownloadFile($cyberchefUrl,$ccfile)
 Expand-Archive -Path $ccfile -DestinationPath ${env:USERPROFILE}\Desktop\CyberChef -Force
 Remove-Item $ccfile -Force
 
 # Download npcap driver
-$npcapVersion = "1.50"
+$npcapVersion = "1.55"
 $npcapUrl = "https://nmap.org/npcap/dist/npcap-${npcapVersion}.exe"
 $npcapFile = Join-Path ${env:USERPROFILE} "Downloads\npcap-${npcapVersion}.exe"
 (New-Object System.Net.WebClient).DownloadFile($npcapUrl, $npcapFile)
+
+# Download Brim
+$brimVersion = Invoke-RestMethod "https://api.github.com/repos/brimdata/brim/releases/latest"
+$brimTag = ($brimVersion).tag_name
+$brimExe = $brimVersion.assets| Where-Object name -match exe$
+$brimUrl = ($brimExe).browser_download_url
+$brimOutFile = Join-Path ${env:USERPROFILE} "Downloads\brim-${brimTag}.exe"
+(New-Object System.Net.WebClient).DownloadFile($brimUrl, $brimOutFile)
 
 # Local settings
 Import-Module ${env:USERPROFILE}\Desktop\wsb\Modules\Sandbox.psd1
