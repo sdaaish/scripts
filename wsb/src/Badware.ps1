5fa97a3d7793d3ea33802c9d74e84f43fb846412
Stig Dahl <stig@charlottendal.net>
2021-12-10 17:29:43 +0100

    Update with log and fix settings

--- wsb/src/Badware.ps1
+++ wsb/src/Badware.ps1
@@ -6,27 +6,38 @@ Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser -Force
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh') -ErrorAction Continue
 
 # Install applications
+$ScoopLog = Join-Path ${env:USERPROFILE} "\Desktop\Scoop.log"
+$scoopApps = @(
+    "windows-terminal"
+    "vscode"
+    "curl"
+    "firefox"
+    "networkminer"
+    "wireshark"
+    "mitmproxy"
+)
 scoop install git
 scoop bucket add extras
-scoop install vscode
-scoop install firefox
-scoop install curl
-scoop install networkminer
-scoop install wireshark
-scoop install windows-terminal
+$scoopApps| Foreach-Object {scoop install $_ *>> $ScoopLog}
 
 # Install VSCode settings
-reg import "${env:USERPROFILE}\scoop\apps\vscode\current\vscode-install-context.reg"
+reg import "${env:USERPROFILE}\scoop\apps\vscode\current\install-context.reg"
 
 # Enable debug of SSL in Firefox, Chrome and Wireshark
 # Create directory, set env variable
-$LogFile = Join-Path ${env:USERPROFILE} "Downloads\ssl\ssl.log"
-New-Item -Path $(Split-Path $Logfile -Parent) -Type Directory
-[System.Environment]::SetEnvironmentVariable("SSLKEYLOGFILE",$LogFile,"Machine")
+$SSLLogFile = Join-Path ${env:USERPROFILE} "Downloads\ssl\ssl.log"
+New-Item -Path $(Split-Path $SSLLogFile -Parent) -Type Directory
+[System.Environment]::SetEnvironmentVariable("SSLKEYLOGFILE",$SSLLogFile,"Machine")
 
 # Install PS-modules
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name DNSClient-PS -Force
+
+# Download NPcap driver
+$npcapVersion = "1.55"
+$npcapUrl = "https://nmap.org/npcap/dist/npcap-${npcapVersion}.exe"
+$npcapFile = Join-Path ${env:USERPROFILE} "Downloads\npcap-${npcapVersion}.exe"
+(New-Object System.Net.WebClient).DownloadFile($npcapUrl, $npcapFile)
 
 # Download CyberChef
 $ccVersion = Invoke-RestMethod "https://api.github.com/repos/gchq/CyberChef/releases/latest"
@@ -36,12 +47,6 @@ $ccfile = Join-Path ${env:USERPROFILE} "Downloads\CyberChef_${ccTag}.zip"
 (New-Object System.Net.WebClient).DownloadFile($cyberchefUrl,$ccfile)
 Expand-Archive -Path $ccfile -DestinationPath ${env:USERPROFILE}\Desktop\CyberChef -Force
 Remove-Item $ccfile -Force
-
-# Download npcap driver
-$npcapVersion = "1.55"
-$npcapUrl = "https://nmap.org/npcap/dist/npcap-${npcapVersion}.exe"
-$npcapFile = Join-Path ${env:USERPROFILE} "Downloads\npcap-${npcapVersion}.exe"
-(New-Object System.Net.WebClient).DownloadFile($npcapUrl, $npcapFile)
 
 # Download Brim
 $brimVersion = Invoke-RestMethod "https://api.github.com/repos/brimdata/brim/releases/latest"
@@ -57,3 +62,6 @@ Set-SBExplorer
 Set-SBWTSettings
 
 & wt.exe
+
+Set-Content -Path $(Join-Path ${env:USERPROFILE} "Desktop\done.txt") -Value "Installation Done"Set-Content -Path $(Join-Path ${env:USERPROFILE} "Desktop\done.txt") -Value "Installation Done"
+
