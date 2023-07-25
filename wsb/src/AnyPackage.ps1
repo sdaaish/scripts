031d56407a0f043c9ab9f53ca33271be782b47fa
Stig Dahl <stig@charlottendal.net>
2023-07-26 00:55:42 +0200

    feat: Add jobs and installation of Nuget

--- wsb/src/AnyPackage.ps1
+++ wsb/src/AnyPackage.ps1
@@ -1,11 +1,46 @@
 # Powershell script to use _inside_ sandbox
 $transcript = Start-Transcript -IncludeInvocationHeader
-
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser -Force
 
 # Local settings
-Import-Module ${env:UserProfile}\Desktop\wsb\Modules\Sandbox.psd1
-Add-SBCertificate -Path ${env:UserProfile}\Desktop\wsb\certificates -Verbose
+Import-Module ${env:USERPROFILE}\Desktop\wsb\Modules\Sandbox.psd1
+Add-SBCertificate -Path ${env:USERPROFILE}\Desktop\wsb\certificates -Verbose
+
+# Examples from https://github.com/jdhitsolutions/WindowsSandboxTools/blob/main/wsbScripts/demo-config.ps1
+$logParams = @{
+    Filepath = "C:\log\setup.txt"
+    Append   = $True
+}
+
+"[$(Get-Date)] Starting $($MyInvocation.MyCommand)" | Out-File @logParams
+
+"[$(Get-Date)] Enabling PSRemoting" | Out-File @logParams
+Enable-PSRemoting -Force -SkipNetworkProfileCheck
+
+"[$(Get-Date)] Install latest nuget package provider" | Out-File @logParams
+Install-PackageProvider -name nuget -force -forcebootstrap -scope allusers
+
+"[$(Get-Date)] Update PackageManagement and PowerShellGet modules" | Out-File @logParams
+Install-Module PackageManagement, PowerShellGet -Force
+
+#run remaining commands in parallel background jobs
+"[$(Get-Date)] Update help" | Out-File @logParams
+Start-Job -Name "Help-Update" -ScriptBlock { Update-Help -Force }
+
+"[$(Get-Date)] Installing default modules" | Out-File @logParams
+Start-Job -Name "Module-Install" -ScriptBlock { Install-Module PSScriptTools, BurntToast -Force }
+
+"[$(Get-Date)] Install Windows Terminal" | Out-File @logParams
+Start-Job -Name "Windows-Terminal" -ScriptBlock { Install-Module WTToolbox -Force ; Install-WTRelease }
+
+#wait for everything to finish
+"[$(Get-Date)] Waiting for jobs to finish" | Out-File @logParams
+Get-Job | Wait-Job
+
+foreach ($job in (Get-Job)) {
+    $result = "Job {0} {1} [{2}]" -f $job.name, $job.state, (New-TimeSpan -Start $job.PSBeginTime -End $job.PSEndTime)
+    "[$(Get-Date)] $result" | Out-File @logParams
+}
 
 # Use AnyPackage and PSResourceGet
 Install-Module -Name Microsoft.PowerShell.PSResourceGet -AllowPrerelease
@@ -24,6 +59,9 @@ Set-SBWTSettings|Out-Null
 Set-SBWinGetSettings|Out-Null
 
 & winget list --source winget --accept-source-agreements
+
+"[$(Get-Date)] Starting Windows Terminal" | Out-File @logParams
+Start-Process wt.exe "-M new-tab -d C:\ -p Windows PowerShell"
 
 $text = @"
 Installation Done.
