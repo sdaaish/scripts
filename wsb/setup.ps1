dd062c2178cbce2bf6a2a618fdb6e7dbacf4140f
Stig Dahl <stig@charlottendal.net>
2024-04-12 14:21:17 +0200

    Pretty print output

--- wsb/setup.ps1
+++ wsb/setup.ps1
@@ -1,7 +1,13 @@
 # Extracts scripts to this folder
-Get-ChildItem -Path $PSScriptRoot\src -Filter gen*.ps1 | Foreach-Object {
-    Write-Host "Generating WSB configuration: $_"
-    & $_
+Get-ChildItem -Path $PSScriptRoot\src -Filter gen*.ps1 | ForEach-Object {
+    $configFile = $_
+    Write-Host "Generating WSB configuration from: $configFile"
+    & $configFile
     Write-Host "Sandbox file in $PSScriptroot\sandboxes"
+
+    Write-Host "Start with:"
+    $name = "& {0}\{1}.wsb" -f $configFile.directory, ($configFile.basename -replace "gen", "" -replace "(conf|config)", "" )
+    Write-Host $name -ForegroundColor Green
+
     [System.Environment]::NewLine
 }
