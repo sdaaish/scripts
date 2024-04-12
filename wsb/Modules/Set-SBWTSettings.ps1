2368b40e8ebb4a380f547707c870f5e8de581e95
Stig Dahl <stig@charlottendal.net>
2024-04-12 13:55:15 +0200

    Update Windows terminal settings

--- wsb/Modules/Set-SBWTSettings.ps1
+++ wsb/Modules/Set-SBWTSettings.ps1
@@ -5,4 +5,9 @@ Function Set-SBWTSettings {
 
     New-Item -Path $WinTerminalHome -Type Directory -Force
     Copy-Item "${HOME}\Desktop\wsb\configs\WindowsTerminal\settings.json" $WinTerminalHome
+
+    # Use Windows Terminal as default
+    $key = 'HKCU:\Console\%%Startup'
+    Set-ItemProperty $key "DelegationTerminal" "{E12CFF52-A866-4C77-9A90-F570A7AA2C6B}"
+    Set-ItemProperty $key "DelegationConsole" "{2EACA947-7F5F-4CFA-BA87-8F7FBEEFBE69}"
 }
